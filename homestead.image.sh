4e3590a3eefc3b9dfbf9e77e6399078a74504a35
Charles Peterson <cpeterson@brokerbin.com>
2017-11-29 15:10:39 -0600

    more checking for requirements.

--- homestead.image.sh
+++ homestead.image.sh
@@ -7,8 +7,13 @@ while getopts "h?:" opt; do
     case "$opt" in
     h|\?)
         echo '
-# requires git and packer
+# requires basic virtualization stuff...
 brew install packer git
+brew cask install vagrant
+brew cask virtualbox-extension-pack
+vagrant plugin install vagrant-bindfs
+vagrant plugin install vagrant-hostsupdater
+vagrant plugin install vagrant-vbguest
 
 # clone this repository
 git clone https://github.com/Artistan/homestead-builder.git ~/homestead-builder
@@ -16,6 +21,8 @@ cd ~/homestead-builder
 
 # run a build for a new image. (auto clones repos) Forces both boxes to build
 ./homestead.image.sh [optional custom-repo/folder]
+
+---------------------- OR ----------------------
 
 # build settler / homestead box and use existing bento box if it is there
 ./build.settler.sh [optional custom-repo/folder]
@@ -35,5 +42,21 @@ echo "** track the build? **"
 echo "tail -f ${BASEDIR}/build.txt"
 echo "** this is going to take awhile **"
 
-./build.bento.sh
-./build.settler.sh
+if which vagrant >/dev/null; then
+    if which VirtualBox >/dev/null; then
+        if which packer >/dev/null; then
+            if which git >/dev/null; then
+                ./build.bento.sh
+                ./build.settler.sh
+            else
+                echo "git is needed to download the repos."
+            fi
+        else
+            echo "packer is needed to make a new image."
+        fi
+    else
+        echo "virtualbox is needed to make a new image."
+    fi
+else
+    echo "vagrant is needed to make a new image."
+fi
